ed3780d18b90c75e70c30eb8b74c5a9200c6c253
ziraqyoung <ziraqyoung@outlook.com>
2020-11-19 14:08:08 +0300

    Create a JSON api endpoint - Create server.js - Add bootstrap and fontawesome css to index.js - Create data.js (initial data for API) - Run React and webservice scripts in parallel

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/css/all.min.css'
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
