3f0f1506fabc049a551b6ae5811976e8eba2ccd0
ziraqyoung <ziraqyoung@outlook.com>
2020-12-01 12:39:46 +0300

    Create Graphql server - Define type definitions(schema) & resolvers - Mount server '/graphql'

--- server.js
+++ server.js
@@ -2,11 +2,18 @@ const express = require("express");
 const jsonServer = require("json-server");
 const chokidar = require("chokidar");
 const cors = require("cors");
+const fs = require("fs");
+const { buildSchema } = require("graphql");
+const { graphqlHTTP } = require('express-graphql');
+
+const queryResolvers = require("./serverQueriesResolver");
+const mutationResolvers = require("./serverMutationsResolver");
 
 const fileName = process.argv[2] || "./data.js";
 const port = process.argv[3] || 3500;
 
 let router = undefined;
+let graph = undefined;
 
 const app = express();
 
@@ -16,6 +23,18 @@ const createServer = () => {
     router = jsonServer.router(
       fileName.endsWith(".js") ? require(fileName)() : fileName
     );
+
+    let schema =
+      fs.readFileSync("./serverQueriesSchema.graphql", "utf-8") +
+      fs.readFileSync("./serverMutationsSchema.graphql", "utf-8");
+    let resolvers = { ...queryResolvers, ...mutationResolvers };
+
+    graph = graphqlHTTP({
+      schema: buildSchema(schema),
+      rootValue: resolvers,
+      graphiql: true,
+      context: { db: router.db },
+    });
   }, 100);
 };
 
@@ -24,8 +43,9 @@ createServer();
 app.use(cors());
 app.use(jsonServer.bodyParser);
 app.use("/api", (req, resp, next) => router(req, resp, next));
+app.use("/graphql", (req, resp, next) => graph(req, resp, next));
 
-chokidar.watch("fileName").on("change", () => {
+chokidar.watch(fileName).on("change", () => {
   console.log("Reloading web service data....");
   createServer();
   console.log("Reloading web service data complete.");
